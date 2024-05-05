f19468054e1df0610c7ab879c82ef9c95620fb52
literrarian <razdolbaika1@gmail.com>
2024-05-05 18:54:46 +0500

    s

--- server/db.js
+++ server/db.js
@@ -2,13 +2,14 @@
 
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-        
-    }
+  //  process.env.DB_NAME,
+  //  process.env.DB_USER,
+  //  process.env.DB_PASSWORD,
+    process.env.DB_URI
+    // {
+    //     dialect: 'postgres',
+    //     host: process.env.DB_HOST,
+    //     port: process.env.DB_PORT
+    //    
+    // }
 )
